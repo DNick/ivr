42e0fe01ec2d2b081c954a2ad0f2504f93252503
Stepan Denisov <sd.denisoff@gmail.com>
2023-11-17 11:38:49 +0300

    change path

--- run.py
+++ run.py
@@ -1,2 +1,2 @@
 import os
-os.system("python App_main/main.py & python App_creators/main.py & python app.py")
+os.system("python App_main/main.py & python App_creators/main.py & python server/app.py")
